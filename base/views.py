57a8c2724a591823338c7b89c8151046d989bfdd
kvo5 <kvo5@uncc.edu>
2024-09-09 00:44:15 -0400

    Add files via upload

--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.contrib import messages
+from django.db.models import Q
+from django.contrib.auth.models import User
+from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
@@ -10,9 +13,33 @@ from .forms import RoomForm
 #     {'id': 3, 'name': 'Frontend developers'},
 # ]
 
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+    
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
 def home(request):
-    rooms = Room.objects.all()
-    context = { 'rooms': rooms }
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    rooms = Room.objects.filter(
+        Q(name__icontains=q) | 
+        Q(description__icontains=q) | 
+        Q(topic__name__icontains=q)
+        )
+    
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+
+    context = { 'rooms': rooms, 'topics': topics, 'room_count': room_count }
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
@@ -50,4 +77,4 @@ def deleteRoom(request, pk):
     if request.method == 'POST':
         room.delete()
         return redirect('home')
-    return render(request, 'base/delete.html', {'obj': 'Room'})
+    return render(request, 'base/delete.html', {'obj':room})
